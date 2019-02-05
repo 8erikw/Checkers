ceffc18d2e913b82e209afaef482fcf163131d41
Erik Wong <erikwong@Eriks-MacBook-Pro.local>
2019-02-05 17:35:51 -0500

    Minor updates

--- src/checkers/model/CheckersModel.py
+++ src/checkers/model/CheckersModel.py
@@ -18,19 +18,61 @@ RED, BLUE = 1, 0
 class CheckersModel:
     def __init__(self):
         """
-            Initializes the legal_moves and pieces
+        Initializes the board
         """
-        self.legal_moves = []
-        self.pieces = []
+        self.size = 8
+        self.state = []
+        self.red_pieces = []
+        self.blue_pieces = []
         self.init_Game()
+        self.state = []
 
     def init_Game(self):
         """
-            Starts a new game.
+        Starts a new game.
+
+        Here is the normal start state of a standard 8 by 8 checkerboard
+
+              0 1 2 3 4 5 6 7
+            0 _ B _ B _ B _ B
+            1 B _ B _ B _ B _
+            2 _ B _ B _ B _ B
+            3 _ _ _ _ _ _ _ _
+            4 _ _ _ _ _ _ _ _
+            5 R _ R _ R _ R _
+            6 _ R _ R _ R _ R
+            7 R _ R _ R _ R _
+
+        """
+        size = self.size
+
+        for i in range(size):
+            if i % 2 == 0:
+                self.red_pieces.append(((5, i), False))
+                self.red_pieces.append(((7, i), False))
+                self.blue_pieces.append(((1, i), False)
+            else:
+                self.blue_pieces.append((0, i))
+                self.blue_pieces.append((2, i))
+                self.red_pieces.append((6, i))
+        self.state.append(self.red_pieces)
+        self.state.append(self.blue_pieces)
+
+    def isTerminalState(self):
+        """
+        Returns whether the current state of the game is over, regardless of who won or lost
+        :return: A boolean value denoting whether the game is over
+        """
+        return len(self.red_pieces) == 0 or len(self.blue_pieces) == 0
+
+    def move(self, piece, move):
         """
 
-
-
-
-    def isGoalState(self, state):
-        if
+        :param piece: A tuple holding the (x,y) coordinates of a piece
+        :param move: A tuple holding the change in coordinates (dx, dy) of the given piece
+        :return: The next state of the board after the move is played
+        """
+        if move[0] != move[1]:
+            raise ValueError(Invalid Move)
+        new_spot = (piece[0] + move[0], piece[1] + move[1])
+        if piece in self.state[1] and ne
--- src/checkers/model/Pieces.py
+++ src/checkers/model/Pieces.py
@@ -1,3 +1,9 @@
+RED, BLUE
+
 class Pieces:
     def __init__(self):
-        
+        self.position = (None, None)
+        self.color =
+        self.king = False
+
+    def promote(self):
