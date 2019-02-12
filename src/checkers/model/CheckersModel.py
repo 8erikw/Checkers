149c5d7aae43fb1cd648d7a916836ca7c980dd6e
Erik Wong <erikwong@Eriks-MacBook-Pro.local>
2019-02-12 17:12:47 -0500

    Updates

--- src/checkers/model/CheckersModel.py
+++ src/checkers/model/CheckersModel.py
@@ -5,6 +5,8 @@
 # Author: Erik Wong
 # Date: February 1, 2019
 
+from Pieces import Pieces
+
 RED, BLUE = 1, 0
 # RED pieces will always go first
 # TO BE IMPLEMENTED : Players can choose which color piece they want to play at the start of the game
@@ -24,8 +26,9 @@ class CheckersModel:
         self.state = []
         self.red_pieces = []
         self.blue_pieces = []
-        self.init_Game()
         self.state = []
+        self.init_Game()
+
 
     def init_Game(self):
         """
@@ -48,15 +51,17 @@ class CheckersModel:
 
         for i in range(size):
             if i % 2 == 0:
-                self.red_pieces.append(((5, i), False))
-                self.red_pieces.append(((7, i), False))
-                self.blue_pieces.append(((1, i), False)
+                self.red_pieces.append(Pieces(5, i, RED))
+                self.red_pieces.append(Pieces(7, i, RED))
+                self.blue_pieces.append(Pieces(1, i, BLUE))
             else:
-                self.blue_pieces.append((0, i))
-                self.blue_pieces.append((2, i))
-                self.red_pieces.append((6, i))
-        self.state.append(self.red_pieces)
+                self.blue_pieces.append(Pieces(0, i, BLUE))
+                self.blue_pieces.append(Pieces(2, i, BLUE))
+                self.red_pieces.append(Pieces(6, i, RED))
+
         self.state.append(self.blue_pieces)
+        self.state.append(self.red_pieces)
+
 
     def isTerminalState(self):
         """
@@ -65,14 +70,60 @@ class CheckersModel:
         """
         return len(self.red_pieces) == 0 or len(self.blue_pieces) == 0
 
-    def move(self, piece, move):
+    def move(self, curr_piece, move):
         """
 
-        :param piece: A tuple holding the (x,y) coordinates of a piece
+        :param curr_piece: A tuple holding the (x,y) coordinates of a piece
         :param move: A tuple holding the change in coordinates (dx, dy) of the given piece
         :return: The next state of the board after the move is played
         """
-        if move[0] != move[1]:
-            raise ValueError(Invalid Move)
-        new_spot = (piece[0] + move[0], piece[1] + move[1])
-        if piece in self.state[1] and ne
+        # Checking so that a non-king piece is not moving backwards
+        if move[1] < 0 and not curr_piece.is_king():
+            raise ValueError("Invalid Move")
+
+        # Checking so that a move strictly diagonal and within a 1 or 2 unit radius
+        if abs(move[1]) != abs(move[0]) and (abs(move[0]) > 2 or move[0] == 0):
+            raise ValueError("Invalid Move")
+
+        # new_spot is the new location of the curr_piece
+        new_spot = (curr_piece[0] + move[0], curr_piece[1] + move[1])
+
+        # Checks if the new_spot is within the board's boundaries
+        if new_spot[0] < 0 or new_spot[1] < 0 or new_spot[0] > self.size - 1 or new_spot[1] > self.size - 1:
+            raise ValueError("Invalid Move")
+
+        # Checking so that moves are valid
+
+        # Checking if a piece is not currently at the new_spot
+        if abs(move[0]) == 1:
+            for team in self.state:
+                for piece in team:
+                    if piece.get_position == new_spot:
+                        raise ValueError("Invalid Move")
+            # After exiting the double for loop, we are sure that there are no pieces occupying the new square,
+            # as no exceptions were raised
+            curr_piece.change_position(new_spot)
+            return
+
+        # A jump is taking place, so an opposing color piece must be in between the jump
+        else:
+            # Calculate the space on the board that is jumped over
+            jump_spot = (curr_piece[0] + move[0] / 2, curr_piece[1] + move[1] / 2)
+
+            # Calculate the current and opposite teams
+            curr_team = curr_piece.get_team()
+            opp_team = RED
+            if curr_team == RED:
+                opp_team = BLUE
+
+            for piece in self.state[opp_team]:
+                if piece.get_position == jump_spot:
+                    curr_piece.change_position(new_spot)
+                    return
+            # Program reaches here if there is no opposing team's piece is in the jump_spot
+            raise ValueError("Invalid Move")
+
+
+
+
+
--- src/checkers/model/Pieces.py
+++ src/checkers/model/Pieces.py
@@ -1,9 +1,28 @@
-RED, BLUE
+RED, BLUE = 0, 1
+
 
 class Pieces:
-    def __init__(self):
-        self.position = (None, None)
-        self.color =
+    def __init__(self, x_position, y_position, team):
+        self.position = (x_position, y_position)
+        self.team = team
         self.king = False
 
     def promote(self):
+        if self.is_king():
+            raise ValueError("Already Promoted")
+        else:
+            self.king = True
+
+    def is_king(self):
+        return self.king
+
+    def change_position(self, dx, dy):
+        x = self.position[0]
+        y = self.position[1]
+        self.position = (x + dx, y + dy)
+
+    def get_position(self):
+        return self.position
+
+    def get_team(self):
+        return self.team
